305a1f4df6d1edf4c887107918fa28bed817856f
JasonKessler <jason.kessler@gmail.com>
2016-10-06 11:48:27 -0700

    Added protocol to HTMLVisualizationAssembly

--- scattertext/test/test_HTMLVisualizationAssembly.py
+++ scattertext/test/test_HTMLVisualizationAssembly.py
@@ -1,18 +1,12 @@
 from unittest import TestCase
 
-from scattertext.viz.HTMLVisualizationAssembly import HTMLVisualizationAssembly
+from scattertext.viz.HTMLVisualizationAssembly import HTMLVisualizationAssembly, InvalidProtocolException
 from scattertext.viz.VizDataAdapter import VizDataAdapter
 import sys
 
 class TestHTMLVisualizationAssembly(TestCase):
 	def test_main(self):
-		words_dict = {"info": {"not_category_name": "Republican", "category_name": "Democratic"},
-		              "data": [{"y": 0.33763837638376387, "term": "crises", "ncat25k": 0,
-		                        "cat25k": 1, "x": 0.0, "s": 0.878755930416447},
-		                       {"y": 0.5, "term": "something else", "ncat25k": 0, "cat25k": 1, "x": 0.0,
-		                        "s": 0.5}]}
-		visualization_data = VizDataAdapter(words_dict)
-		assembler = HTMLVisualizationAssembly(visualization_data)
+		assembler = self.make_assembler()
 		html = assembler.to_html()
 		if sys.version_info.major == 2:
 			self.assertEqual(type(html), unicode)
@@ -20,6 +14,35 @@ class TestHTMLVisualizationAssembly(TestCase):
 			self.assertEqual(type(html), str)
 		self.assertFalse('<!-- INSERT SCRIPT -->' in html)
 		self.assertTrue('Republican' in html)
+
+	def test_protocol_is_https(self):
+		html = self.make_assembler().to_html(protocol='https')
+		self.assertTrue('https://' in html)
+		self.assertFalse('http://' in html)
+
+	def test_protocol_is_http(self):
+		html = self.make_assembler().to_html(protocol='http')
+		self.assertFalse('https://' in html)
+		self.assertTrue('http://' in html)
+
+	def test_protocol_defaults_to_http(self):
+		self.assertEqual(self.make_assembler().to_html(protocol='http'),
+		                 self.make_assembler().to_html(),)
+
+	def test_raise_invalid_protocol_exception(self):
+		with self.assertRaisesRegexp(InvalidProtocolException,
+		                             "Invalid protocol: 'ftp'.  Protocol must be either http or https."):
+			self.make_assembler().to_html(protocol='ftp')
+
+	def make_assembler(self):
+		words_dict = {"info": {"not_category_name": "Republican", "category_name": "Democratic"},
+		              "data": [{"y": 0.33763837638376387, "term": "crises", "ncat25k": 0,
+		                        "cat25k": 1, "x": 0.0, "s": 0.878755930416447},
+		                       {"y": 0.5, "term": "something else", "ncat25k": 0, "cat25k": 1, "x": 0.0,
+		                        "s": 0.5}]}
+		visualization_data = VizDataAdapter(words_dict)
+		assembler = HTMLVisualizationAssembly(visualization_data)
+		return assembler
 
 
 
--- scattertext/viz/HTMLVisualizationAssembly.py
+++ scattertext/viz/HTMLVisualizationAssembly.py
@@ -1,6 +1,10 @@
 import pkgutil
 
 
+class InvalidProtocolException(Exception):
+	pass
+
+
 class HTMLVisualizationAssembly:
 	def __init__(self, visualization_data):
 		'''
@@ -8,13 +12,18 @@ class HTMLVisualizationAssembly:
 		'''
 		self._visualization_data = visualization_data
 
-	def to_html(self):
-		html_file = pkgutil.get_data('scattertext', 'data/viz/scattertext.html').decode('utf-8')
+	def to_html(self, protocol='http'):
+		if protocol not in ('https', 'http'):
+			raise InvalidProtocolException("Invalid protocol: '" + protocol + "'.  Protocol must be either http or https.")
+		html_file = pkgutil.get_data('scattertext',
+		                             'data/viz/scattertext.html').decode('utf-8')
 		scripts = '\n'.join([pkgutil.get_data('scattertext',
 		                                      'data/viz/scripts/' + script_name).decode('utf-8')
 		                     for script_name in ['range-tree.js',
 		                                         'main.js']])
 		viz_data_in_javascript_command_form = self._visualization_data.to_javascript()
 		scripts += '\n' + viz_data_in_javascript_command_form
-		html_file = html_file.replace('<!-- INSERT SCRIPT -->', scripts, 1)
+		html_file = (html_file
+		             .replace('<!-- INSERT SCRIPT -->', scripts, 1)
+		             .replace('http://', protocol + '://'))
 		return html_file
