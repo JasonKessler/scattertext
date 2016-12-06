59c0f414fa8f4b11af2a7306e420ea8efe513cbc
JasonKessler <jason.kessler@gmail.com>
2016-12-05 18:34:14 -0800

    Incrementing version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='scattertext',
-      version='0.0.1.8.11',
+      version='0.0.1.9.0',
       description='An NLP libarary to help find interesting terms in small to medium-sized corpora.',
       url='https://github.com/JasonKessler/scattertext',
       author='Jason Kessler',
