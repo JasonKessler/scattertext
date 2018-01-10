d68104c803afb637df71f166c2b8852e92228f83
JasonKessler <jason.kessler@gmail.com>
2018-01-10 11:40:34 -0800

    Version: 0.0.2.16.1... simplifying test

--- scattertext/test/test_termDocMatrixFromScikit.py
+++ scattertext/test/test_termDocMatrixFromScikit.py
@@ -1,23 +1,29 @@
 from unittest import TestCase
 
+import numpy as np
+
 from scattertext import TermDocMatrixFromScikit
+from scattertext.indexstore import IndexStore
+from scattertext.test.test_semioticSquare import get_docs_categories_semiotic
 
 
 class TestTermDocMatrixFromScikit(TestCase):
 	def test_build(self):
-		from sklearn.datasets import fetch_20newsgroups
 		from sklearn.feature_extraction.text import CountVectorizer
-		newsgroups_train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'))
+		categories, docs = get_docs_categories_semiotic()
+		idx_store = IndexStore()
+		y = np.array([idx_store.getidx(c) for c in categories])
+		print(categories, y)
 		count_vectorizer = CountVectorizer()
-		X_counts = count_vectorizer.fit_transform(newsgroups_train.data)
+		X_counts = count_vectorizer.fit_transform(docs)
 		term_doc_mat = TermDocMatrixFromScikit(
 			X=X_counts,
-			y=newsgroups_train.target,
+			y=y,
 			feature_vocabulary=count_vectorizer.vocabulary_,
-			category_names=newsgroups_train.target_names).build()
-		self.assertEqual(term_doc_mat.get_categories()[:2], ['alt.atheism', 'comp.graphics'])
+			category_names=idx_store.values()).build()
+		self.assertEqual(term_doc_mat.get_categories()[:2], ['hamlet', 'jay-z/r. kelly'])
 		self.assertEqual(term_doc_mat
 		                 .get_term_freq_df()
-		                 .assign(score=term_doc_mat.get_scaled_f_scores('alt.atheism'))
+		                 .assign(score=term_doc_mat.get_scaled_f_scores('hamlet'))
 		                 .sort_values(by='score', ascending=False).index.tolist()[:5],
-		                 ['atheism', 'atheists', 'islam', 'atheist', 'belief'])
+		                 ['that', 'march', 'did', 'majesty', 'sometimes'])
