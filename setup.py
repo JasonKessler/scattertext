99aad97bb204dabb3be796e505c63b08600dc896
JasonKessler <Jason.Kessler@cdk.com>
2018-05-16 12:37:57 -0700

    Adding docs, and fixing setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='scattertext',
-      version='0.0.2.7.1',
+      version='0.0.2.27.1',
       description='An NLP package to visualize interesting terms in text.',
       url='https://github.com/JasonKessler/scattertext',
       author='Jason Kessler',
