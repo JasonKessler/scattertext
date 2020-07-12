17941489d7e63192758f0a728208a2d88f3f5872
Jason Kessler <JasonKessler@users.noreply.github.com>
2020-07-12 16:50:28 -0700

    Adding lemma demo

---
+++ demo_lemmas.py
@@ -0,0 +1,25 @@
+import scattertext as st
+import spacy
+nlp = spacy.load('en')
+
+df = st.SampleCorpora.ConventionData2012.get_data().assign(
+    parse=lambda df: df.text.apply(nlp)
+)
+
+corpus = st.CorpusFromParsedDocuments(
+    df, category_col='party', parsed_col='parse',
+    feats_from_spacy_doc=st.FeatsFromSpacyDoc(use_lemmas=True)
+).build().get_unigram_corpus().compact(st.AssociationCompactor(2000))
+
+html = st.produce_scattertext_explorer(
+    corpus,
+    category='democrat',
+    category_name='Democratic',
+    not_category_name='Republican',
+    minimum_term_frequency=0, pmi_threshold_coefficient=0,
+    width_in_pixels=1000, metadata=corpus.get_df()['speaker'],
+    transform=st.Scalers.dense_rank,
+    max_overlapping=3
+)
+open('./demo_lemmas.html', 'w').write(html)
+print('open ./demo_lemmas.html in Chrome')
