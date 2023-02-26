a442f9c7db18e1fe08538575eeece49d8787afec
Jason Kessler <JasonKessler@users.noreply.github.com>
2023-02-27 00:50:33 +0100

    Version 0.1.15
    HTTP/HTTPS Issue, Version number

--- scattertext/Common.py
+++ scattertext/Common.py
@@ -32,8 +32,8 @@ ROTTEN_TOMATOES_DATA_FULL_URL = 'https://gitcdn.xyz/repo/JasonKessler/scattertex
 GENERAL_INQUIRER_URL = 'http://www.wjh.harvard.edu/~inquirer/inqtabs.txt'
 
 # For sample corpus
-DEFAULT_D3_URL = 'http://cdnjs.cloudflare.com/ajax/libs/d3/4.6.0/d3.min.js'
-DEFAULT_D3_SCALE_CHROMATIC = 'http://d3js.org/d3-scale-chromatic.v1.min.js'
+DEFAULT_D3_URL = 'https://cdnjs.cloudflare.com/ajax/libs/d3/4.6.0/d3.min.js'
+DEFAULT_D3_SCALE_CHROMATIC = 'https://d3js.org/d3-scale-chromatic.v1.min.js'
 SPACY_ENTITY_TAGS = ['person', 'norp', 'facility', 'org', 'gpe',
                      'loc', 'product', 'event', 'work_of_art', 'language',
                      'type', 'date', 'time', 'percent', 'money', 'quantity',
